d331045528ddc1fa6949cc370da62be503d1c5d3
chrishibby75 <chrishibby75@gmail.com>
2018-11-02 14:14:37 -0600

    submitting

--- liri.js
+++ liri.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 
 //vars
-var keys = require("./keys");
+var keys = require("./keys.js");
 var fs = require("fs");
 var request = require("request");
-var bandsintown = require("bandsintown");
+var bandsintown = require("bandsintown")("codingbootcamp");
 //var Spotify = require('spotify-web-api-node')
 var Spotify = require("node-spotify-api");
 //creates log.txt file
 var filename = "./log.txt";
 //NPM module used to write output to console and log.txt simulatneously
-var log = require('simple-node-logger').createSimpleFileLogger(filename);
-log.setLevel('all');
+var log = require("simple-node-logger").createSimpleFileLogger(filename);
+log.setLevel("all");
 
 //argv[2] chooses users actions; argv[3] is input parameter, ie; movie title
 var userCommand = process.argv[2];
@@ -22,22 +22,41 @@ for (var i = 4; i < process.argv.length; i++) {
     secondCommand += '+' + process.argv[i];
 }
 
+function mySwitch(userCommand) {
+    switch (userCommand) {
+
+        case "concert-this":
+            getConcert();
+            break;
+
+        case "spotify-this-song":
+            getSpotify();
+            break;
+
+        case "movie-this":
+            getMovie();
+            break;
+
+        case "do-what-it-says":
+            DoIt();
+            break;
+    }
 //get spotify keys
 
-var spotify = new spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 var getArtistsName = function (artist) {
     return artist.name;
 };
 
-var getSpotity = function (songName) {
+function getSpotify (songName) {
     if (songName === undefined) {
         songName = "I want it that way";
     }
     spotify.search(
         {
             type: 'track',
-            query: userCommand
+            query: secondCommand
         },
         function (err, data) {
             if (err) {
@@ -57,55 +76,70 @@ var getSpotity = function (songName) {
     );
 };
 
-function mySwitch(userCommand) {
-    switch(userCommand) {
-
-        case "concert-this":
-        getConcert();
-        break;
-
-        case "spotify-this-song":
-        getSpotify();
-        break;
-
-        case "movie-this":
-        getMovie();
-        break;
-
-        case "do-what-it-says":
-        DoIt();
-        break;
-    }
-
     function getConcert() {
-        var concertName = secondCommand;
+        if (action === 'concert-this') {
+            var artistName = "";
+            for (var i = 3; i < process.argv.length; i++) {
+                artistName += process.argv[i];
+            }
+            console.log(artistName);
+        }
+        else {
+            artistName = parameter;
+        }
 
-        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-        request(queryUrl, function(error, response, artist) {
-            var artist = JSON.parse(artist);
-            logOutput('===================Artist info===================');
-            logOutput('')
-        })
+
+
+        var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codecademy";
+
+
+        request(queryUrl, function (error, response, body) {
+
+            if (!error && response.statusCode === 200) {
+
+                var JS = JSON.parse(body);
+                for (i = 0; i < JS.length; i++) {
+                    var dTime = JS[i].datetime;
+                    var month = dTime.substring(5, 7);
+                    var year = dTime.substring(0, 4);
+                    var day = dTime.substring(8, 10);
+                    var dateForm = month + "/" + day + "/" + year
+
+                    logIt("\n---------------------------------------------------\n");
+
+
+                    console.log("Date: " + dateForm);
+                    console.log("Name: " + JS[i].venue.name);
+                    console.log("City: " + JS[i].venue.city);
+                    if (JS[i].venue.region !== "") {
+                        console.log("Country: " + JS[i].venue.region);
+                    }
+                    console.log("Country: " + JS[i].venue.country);
+                    console.log("\n---------------------------------------------------\n");
+
+                }
+            }
+        });
     }
 
     function getMovie() {
         var movieName = secondCommand;
 
         var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
-        request(queryUrl, function(error, response, body) {
+        request(queryUrl, function (error, response, body) {
             if (!error && response.statusCode === 200) {
                 var body = JSON.parse(body);
-                logOutput('=================Movie Info=================');
-                logOutput("Title: " + body.Title);
-                logOutput("Release Year: " + body.Year);
-                logOutput("IMdB rating: " + body.imdbRating);
-                logOutput("Country: " + body.Country);
-                logOutput("Language: " + body.Language);
-                logOutput("Plot: " + body.Plot);
-                logOutput("Actors: " + body.Actors);
-                logOutput("Rotten Tomatoes rating: " + body.Ratings[2].Value);
-                logOutput("Rotten Tomatoes URL: " + body.tomatoURL);
-                logOutput("==================The End==================");
+                logIt("=================Movie Info=================");
+                logIt("Title: " + body.Title);
+                logIt("Release Year: " + body.Year);
+                logIt("IMdB rating: " + body.imdbRating);
+                logIt("Country: " + body.Country);
+                logIt("Language: " + body.Language);
+                logIt("Plot: " + body.Plot);
+                logIt("Actors: " + body.Actors);
+                logIt("Rotten Tomatoes rating: " + body.Ratings[2].Value);
+                logIt("Rotten Tomatoes URL: " + body.tomatoURL);
+                logIt("==================The End==================");
 
             } else {
                 console.log("Error occurred")
@@ -117,12 +151,12 @@ function mySwitch(userCommand) {
             }
         })
     }
-        function doIt() {
-            fs.readFile("random.txt", "utf8", function(error, data){
-                if (!error);
-                console.log(data.toString());
-                var cmds = data.toString().split(',');
-            });
-        }
+    function doIt() {
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (!error);
+            console.log(data.toString());
+            var cmds = data.toString().split(',');
+        });
+    }
 }
-    mySwitch(userCommand);
+mySwitch(userCommand);
